d92dcffe2bca7a23733c5a05bd9c5297ee5c3652
nassir07 <nassir.cherif@ept.ucar.tn>
2025-07-07 10:34:15 +0100

    first commit

--- python_template/main.py
+++ python_template/main.py
@@ -1,22 +1,26 @@
 """Contain main module."""
-
-import logging
-from logging import config
-
+import logging.config
 from python_template.application.my_module import MyModule
+import sys
+import os
 
-# Load config
-config.fileConfig("../log.conf")
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Test logging
+# 1. Construire le chemin absolu vers log.conf
+log_config_path = os.path.join(os.path.dirname(__file__), '../log.conf')
+
+# 2. Charger la configuration de logging
+logging.config.fileConfig(log_config_path, disable_existing_loggers=False)
+
+# 3. Créer le logger
 logger = logging.getLogger(__name__)
-logger.info("This will be logged to console and file")
-
+logger.info("Logger initialized correctly.")
 
 def main():
-    """Implement main function."""
+    logger.info("Main function started.")
     MyModule.my_function()
-
+    logger.info("Main function finished.")
 
 if __name__ == "__main__":
     main()
+
