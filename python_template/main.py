3462e19c6c01d6a7c068dd9e79e6de975434a839
nassir07 <nassir.cherif@ept.ucar.tn>
2025-07-07 11:45:15 +0100

    ✅ Fix flake8 D401 and W391 in main.py

--- python_template/main.py
+++ python_template/main.py
@@ -1,4 +1,4 @@
-"""Contain main module."""
+"""Run the main application logic."""
 
 import logging.config
 import os
@@ -8,19 +8,15 @@ from python_template.application.my_module import MyModule
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# 1. Construire le chemin absolu vers log.conf
 log_config_path = os.path.join(os.path.dirname(__file__), '../log.conf')
-
-# 2. Charger la configuration de logging
 logging.config.fileConfig(log_config_path, disable_existing_loggers=False)
 
-# 3. Créer le logger
 logger = logging.getLogger(__name__)
 logger.info("Logger initialized correctly.")
 
 
 def main():
-    """Main entry point of the application."""
+    """Run the main application logic."""
     logger.info("Main function started.")
     MyModule.my_function()
     logger.info("Main function finished.")
@@ -28,5 +24,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
